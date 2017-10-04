5ff77681700881c34683e21871f479ac41f62caa
亚奇 许 <xuyaqi888@live.cn>
2017-10-04 20:14:16 +0800

    New Building

--- TSDMClientWPF/Views/ReplyPage.xaml.cs
+++ TSDMClientWPF/Views/ReplyPage.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TSDMClientWPF
+namespace LaCODESoftware.Tsdm.Views
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// ReplyPage.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ReplyPage : Page
     {
-        public MainWindow()
+        public ReplyPage()
         {
             InitializeComponent();
         }
